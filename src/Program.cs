418b8124e615402a6e6b7cd76f0f47a82f9369bc
Greenlock <pokedoxysm@gmail.com>
2015-08-14 16:02:43 -0400

    Initial commit, part 2

---
+++ src/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Modicite.CLI {
+
+    class Placeholder {
+
+        static void Main(string[] args) {
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            Console.WriteLine("Modicite - An API and toolkit for modding Magicite");
+            Console.WriteLine("by Greenlock / Nathan2055");
+            Console.ResetColor();
+            Console.ReadKey();
+        }
+    }
+}
